8581bafdd6a1d8259ac313aa45bac9ecc8cb9132
videotutorialdev <12bedevnetwork@gmail.com>
2023-01-08 01:30:59 +0700

    add: first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="container py-4">
+      <h1 className="mb-0">React Form</h1>
+      <p>Learn how to use form in react</p>
+      <button className="btn btn-primary">
+        <em className="fas fa-plus"></em> Add
+      </button>
+    </div>
+  );
+}
+
+export default App;
